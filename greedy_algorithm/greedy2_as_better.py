ca7bd7be6cc5ff0b53eea1a8306962adc50fa071
yeongjun Kim <dudwns650395@gmail.com>
2021-04-02 21:35:57 +0900

    update: 숫자 카드 게임 문제 및 다른 풀이

---
+++ greedy_algorithm/greedy2_as_better.py
@@ -0,0 +1,14 @@
+# 문제는 greedy2.py에 작성되어 있습니다.
+
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+  data = list(map(int, input().split()))
+  # 현재 줄에서 가장 작은 수 찾기
+  min_value = min(data)
+  # '가장 작은 수'들 중에서 가장 큰 수 찾기
+  result = max(result, min_value)
+
+print(result)
